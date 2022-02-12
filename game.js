f3bcd14382de59810d9875111ae868e4f34e06af
marurevi <maryrev@hotmail.com>
2022-02-11 22:12:22 -0300

    Score Table Adition

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 const computerDisplay = document.getElementById('computer-choice');
 const playerDisplay = document.getElementById('your-choice');
 const resultDisplay = document.getElementById('result');
+let computerScoreDisplay = document.getElementById('computerScore');
+let playerScoreDisplay = document.getElementById('playerScore');
 const choices = ['Rock', 'Paper', 'Scissors'];
-let result;
+let result, playerScore = 0, computerScore = 0;
 
 
 document.addEventListener('click', (e) => {
@@ -10,8 +12,16 @@ document.addEventListener('click', (e) => {
     computer = computerChoice(); 
     computerDisplay.innerHTML = computer;
     playerDisplay.innerHTML = player.charAt(0).toUpperCase() + player.slice(1);
-    resultDisplay.innerHTML = getResult();
+    result = getResult();
+    resultDisplay.innerHTML = result;
+    if (result == 'You lost!') { computerScore+=1; }
+    if (result == 'You win!') { playerScore+=1; }
+    computerScoreDisplay.innerHTML = computerScore;
+    playerScoreDisplay.innerHTML = playerScore;
+    
 })
+game()
+
 
 function computerChoice () {
     let computer = choices[Math.floor(Math.random(choices)*3)];
@@ -42,4 +52,33 @@ function getResult(){
     return result;
 }
 
+
 // Creation of Score Table
+
+function game () {
+    let winner = '';
+    let sum = 0;
+    document.addEventListener('click', (e) => {
+        e = Math.max (playerScore, computerScore);
+        sum = playerScore + computerScore;
+
+        if (e == playerScore) { 
+            winner = 'player';
+        }else {
+            winner = 'computer';
+        }
+        
+        if (sum > 5) {
+            if (winner == 'player') {
+                alert( 'YOU WIN THIS GAME!!!');
+            } else {
+                alert ('GAME OVER');
+            }
+            playerScore = 0;
+            computerScore = 0;
+        } 
+    }) 
+}
+
+
+
